ff3ffa0e2599b07e6e81c29288db32d0a96e2b1f
Kent Ntwari <designerjehovah@gmail.com>
2025-12-20 22:24:58 -0500

    feat: init i18n, configure shadcn and clerk module:

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,8 +6,34 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ["./app/assets/css/main.css"],
-  modules: ["@formkit/auto-animate", "@vee-validate/nuxt", "@nuxtjs/i18n"],
+  modules: [
+    "@formkit/auto-animate",
+    "@vee-validate/nuxt",
+    "@nuxtjs/i18n",
+    "@clerk/nuxt",
+    "shadcn-nuxt",
+    "@nuxt/icon",
+  ],
   vite: {
     plugins: [tailwindcss()],
+  },
+  icon: {
+    mode: "css",
+    cssLayer: "base",
+  },
+  shadcn: {
+    prefix: "UI",
+    componentDir: "@/components/ui",
+  },
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      { code: "en", name: "English", file: "en.json" },
+      { code: "fr", name: "Français", file: "fr.json" },
+    ],
+    strategy: "no_prefix",
+  },
+  imports: {
+    dirs: ["mvc"],
   },
 });
