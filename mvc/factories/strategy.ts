871f56ed9afc8217ed84a14095b8ff3cce9dfa51
Kent Ntwari <designerjehovah@gmail.com>
2026-01-18 12:10:48 -0500

    refactor:

--- mvc/factories/strategy.ts
+++
@@ -1,9 +0,0 @@
-import { CreateUserFactory } from "../factories/user";
-
-export interface IFactoryStrategyMap {
-  readonly registerUser: CreateUserFactory;
-}
-
-export const FactoryStrategy: IFactoryStrategyMap = {
-  registerUser: new CreateUserFactory(),
-};
