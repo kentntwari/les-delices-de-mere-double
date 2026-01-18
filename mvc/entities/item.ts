bfd87d0c14cb32105856adb80e292e3405a2d75c
Kent Ntwari <designerjehovah@gmail.com>
2026-01-18 12:07:46 -0500

    refacor: dichotomize items into ordered and menu parts

--- mvc/entities/item.ts
+++ mvc/entities/item.ts
@@ -1,19 +1,43 @@
-import type { MenuItem as ItemModel } from "@prisma/client";
+import type { OrderedItemModel, MenuItemModel } from "../repository/item";
 
 import s from "slugify";
 
-export class ItemEntity implements Pick<ItemModel, "id" | "title" | "price"> {
-  protected _slug: string = "unspecified-slug";
+export class OrderedItemEntity implements Partial<OrderedItemModel> {
+  constructor(
+    public readonly id: string,
+    public readonly orderId: string,
+    public readonly title: string,
+    public readonly slug: string,
+    public readonly unitPrice: number,
+    public quantity: number
+  ) {}
 
+  get total(): number {
+    return this.unitPrice * this.quantity;
+  }
+}
+
+export class MenuItemEntity
+  implements Omit<MenuItemModel, "description" | "slug">
+{
   constructor(
     public readonly id: string,
     public title: string,
-    public price: number
+    public slug: string | undefined,
+    public unitPrice: number
   ) {
-    this.generateSlug();
+    const isSlugEmpty = !this.slug || this.slug.trim() === "";
+    if (isSlugEmpty) this.generateSlug();
   }
 
   generateSlug(): void {
-    this._slug = s(this.title, { lower: true, strict: true });
+    this.slug = s(this.title, { lower: true, strict: true });
+  }
+
+  static generateID(): string {
+    const buf = new Uint32Array(1);
+    crypto.getRandomValues(buf);
+    const n = (buf.at(0) ?? 0) % 10000;
+    return `MI${n.toString().padStart(4, "0")}`;
   }
 }
