b87db8dd30b53899d8be11102d701ed344f9f8da
Kent Ntwari <designerjehovah@gmail.com>
2026-01-18 11:51:37 -0500

    styles: refine styling

--- app/components/ui/contextMenu/ContextMenuSubContent.vue
+++ app/components/ui/contextMenu/ContextMenuSubContent.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
-import type { DropdownMenuSubContentEmits, DropdownMenuSubContentProps } from "reka-ui"
-import type { HTMLAttributes } from "vue"
-import { reactiveOmit } from "@vueuse/core"
-import {
-  ContextMenuSubContent,
-  useForwardPropsEmits,
-} from "reka-ui"
-import { cn } from '@/utils/cn'
+  import type {
+    DropdownMenuSubContentEmits,
+    DropdownMenuSubContentProps,
+  } from "reka-ui";
+  import type { HTMLAttributes } from "vue";
+  import { reactiveOmit } from "@vueuse/core";
+  import { ContextMenuSubContent, useForwardPropsEmits } from "reka-ui";
+  import { cn } from "@/utils/cn";
 
-const props = defineProps<DropdownMenuSubContentProps & { class?: HTMLAttributes["class"] }>()
-const emits = defineEmits<DropdownMenuSubContentEmits>()
+  const props = defineProps<
+    DropdownMenuSubContentProps & { class?: HTMLAttributes["class"] }
+  >();
+  const emits = defineEmits<DropdownMenuSubContentEmits>();
 
-const delegatedProps = reactiveOmit(props, "class")
+  const delegatedProps = reactiveOmit(props, "class");
 
-const forwarded = useForwardPropsEmits(delegatedProps, emits)
+  const forwarded = useForwardPropsEmits(delegatedProps, emits);
 </script>
 
 <template>
@@ -22,8 +24,8 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
     v-bind="forwarded"
     :class="
       cn(
-        'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] origin-(--reka-context-menu-content-transform-origin) overflow-hidden rounded-md border p-1 shadow-lg',
-        props.class,
+        'bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 min-w-[8rem] origin-(--reka-context-menu-content-transform-origin) overflow-hidden rounded-md border border-neutral-grey-600 p-1 shadow-lg',
+        props.class
       )
     "
   >
