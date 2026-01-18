7a10f2ea7d5fdbfd9d4e732e8ced8738db342e5c
Kent Ntwari <designerjehovah@gmail.com>
2026-01-18 12:11:11 -0500

    refactor:

--- mvc/mapper/base.ts
+++ mvc/mapper/base.ts
@@ -1,5 +1,6 @@
-export abstract class BaseMapper<TEntity, TModel> {
+export abstract class BaseMapper<TEntity, TDTO, TModel> {
   abstract toEntity(data: TModel): TEntity;
   // TODOD: Implement full toModel
   abstract toSafeModel(entity: TEntity): Partial<TModel>;
+  abstract toDto(entity: TEntity): TDTO;
 }
