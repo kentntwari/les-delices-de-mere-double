06791fec4eb154e674bb0717353bb191e9671da7
Kent Ntwari <designerjehovah@gmail.com>
2026-03-29 17:19:20 -0400

    feat: implement order preview count

---
+++ server/api/order/[orderId]/metadata/count.get.ts
@@ -0,0 +1,30 @@
+import { createRequestLogger } from "~~/server/utils/logger";
+import { OrderController } from "~~/mvc/controllers/order";
+import { OrderRepository } from "~~/mvc/repository/order";
+
+const log = createRequestLogger("server.api.order.[orderId].meta.count.get.ts");
+
+export default defineEventHandler(async (event) => {
+  try {
+    const { orderId } = event.context.params as { orderId: string };
+
+    log.info(
+      event.path,
+      event.method,
+      {
+        param: { orderId },
+      },
+      "GET REQUEST RECEIVED: Getting order count metadata",
+    );
+
+    const r = await new OrderController(toWebRequest(event)).handleIntent(
+      "get-order-count-metadata",
+      orderId,
+    );
+    return treatResponses(event, r) as {
+      data: ReturnType<OrderRepository["getCountMetadata"]>;
+    };
+  } catch (error) {
+    treatErrors(error);
+  }
+});
