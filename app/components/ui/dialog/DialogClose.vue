5b5b35cf537bf012bb2102aa9d61deae5b352e92
Kent Ntwari <designerjehovah@gmail.com>
2026-03-29 16:55:17 -0400

    refactor: styling and overlay

--- app/components/ui/dialog/DialogClose.vue
+++ app/components/ui/dialog/DialogClose.vue
@@ -11,8 +11,12 @@
   <DialogClose
     data-slot="dialog-close"
     v-bind="props"
-    class="size-7 rounded-[6px] border border-neutral-grey-700 cursor-pointer"
+    class="size-7 rounded-[6px] border hover:border-2 border-neutral-grey-700 hover:border-neutral-grey-900 cursor-pointer"
   >
-    <X :size="24" :stroke-width="1" class="text-neutral-grey-700" />
+    <X
+      :size="24"
+      :stroke-width="1"
+      class="text-neutral-grey-700 hover:text-neutral-grey-900"
+    />
   </DialogClose>
 </template>
--- app/components/ui/dialog/DialogContent.vue
+++ app/components/ui/dialog/DialogContent.vue
@@ -21,11 +21,13 @@
       DialogContentProps & {
         class?: HTMLAttributes["class"];
         showCloseButton?: boolean;
+        showOverlay?: boolean;
       }
     >(),
     {
       showCloseButton: true,
-    }
+      showOverlay: true,
+    },
   );
   const emits = defineEmits<DialogContentEmits>();
 
@@ -36,14 +38,14 @@
 
 <template>
   <DialogPortal>
-    <DialogOverlay />
+    <DialogOverlay v-show="showOverlay" />
     <DialogContent
       data-slot="dialog-content"
       v-bind="{ ...$attrs, ...forwarded }"
       :class="
         cn(
           'bg-neutral-grey-200 grid grid-rows-[auto_1fr_auto] data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open] :zoom-in-95 fixed top-4 right-4 z-50 lg:min-h-[calc(100vh-2rem)] w-full lg:max-w-180 rounded-3xl border shadow-lg duration-200 sm:max-w-lg',
-          props.class
+          props.class,
         )
       "
     >
