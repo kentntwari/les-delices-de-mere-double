2fe51fb74a6f009d1f083aa3b45af9e1182da153
Kent Ntwari <designerjehovah@gmail.com>
2026-03-29 17:19:01 -0400

    feat: implement logs

---
+++ server/api/order/[orderId]/logs.get.ts
@@ -0,0 +1,44 @@
+import { createRequestLogger } from "~~/server/utils/logger";
+import { OrderController } from "~~/mvc/controllers/order";
+import { type TOrderLogDTO } from "~~/mvc/mapper/order";
+
+const log = createRequestLogger("server.api.order.[orderId].logs.get.ts");
+
+export default defineCachedEventHandler(
+  async (event) => {
+    let id: string = "UNKNOWN_ORDER_ID";
+
+    try {
+      const { orderId } = event.context.params as { orderId: string };
+
+      id = orderId;
+
+      log.info(
+        event.path,
+        event.method,
+        {
+          param: { orderId },
+        },
+        "GET REQUEST RECEIVED: Getting order logs",
+      );
+
+      const r = await new OrderController(toWebRequest(event)).handleIntent(
+        "get-logs",
+        orderId,
+      );
+      return treatResponses(event, r) as { data: TOrderLogDTO[] };
+    } catch (error) {
+      treatErrors(error);
+    }
+  },
+  {
+    name: "logs",
+    maxAge: 60 * 60 * 24 * 7, // 7 days
+    swr: false,
+    getKey: (event) => {
+      const { orderId } = event.context.params as { orderId: string };
+      if (!orderId) return "orderId_missing";
+      return `orderId_${encodeURIComponent(orderId.toLowerCase())}`;
+    },
+  },
+);
