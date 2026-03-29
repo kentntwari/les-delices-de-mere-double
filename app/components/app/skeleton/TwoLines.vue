d1acd268f1f876319a4da9dcaec68c54364ffb62
Kent Ntwari <designerjehovah@gmail.com>
2026-03-29 16:53:53 -0400

    feat: change directory

---
+++ app/components/app/skeleton/TwoLines.vue
@@ -0,0 +1,8 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="*:h-4 *:bg-neutral-grey-500 animate-pulse rounded-xs space-y-2">
+    <div class="w-20"></div>
+    <div class="w-40"></div>
+  </div>
+</template>
