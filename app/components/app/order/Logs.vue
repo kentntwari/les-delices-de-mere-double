a913664815e107555b55812b357fac629cdb704e
Kent Ntwari <designerjehovah@gmail.com>
2026-03-29 16:54:20 -0400

    feat:implement logs UI

---
+++ app/components/app/order/Logs.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+  import type { TOrderLogDTO } from "~~/mvc/mapper/order";
+
+  import { FileTextIcon } from "lucide-vue-next";
+
+  const props = defineProps<{
+    logs: TOrderLogDTO[] | null;
+  }>();
+</script>
+
+<template>
+  <ul class="grid grid-rows-[1fr_auto] *:not-last:mb-4">
+    <li
+      v-for="log in logs"
+      :key="log.createdAt"
+      class="w-full flex items-center gap-1 *:text-primary-500"
+    >
+      <FileTextIcon :size="20" class="text-gray-500" />
+      <p class="text-sm">{{ log.message }}</p>
+    </li>
+  </ul>
+</template>
