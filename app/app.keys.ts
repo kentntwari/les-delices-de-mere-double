b855249867fac2fbddff9f3909c8783dd2ea89c4
Kent Ntwari <designerjehovah@gmail.com>
2026-03-29 17:01:26 -0400

    feat: implement order related keys

--- app/app.keys.ts
+++ app/app.keys.ts
@@ -4,6 +4,9 @@ export const INJECT_FIRST_INTERACTION = Symbol("INJECT_FIRST_INTERACTION");
 
 /* CLIENT FETCH KEYS */
 export const GET_ORDERS_KEY = "__GET__ORDERS__";
+export const GET_ORDER_FULL_DETAILS_KEY = "__GET__ORDER__FULL__DETAILS__";
+export const GET_ORDER_LOGS_KEY = "__GET__ORDER__LOGS__";
+export const GET_ORDER_COMMENTS_KEY = "__GET__ORDER__COMMENTS__";
 export const GET_MENU_ITEMS_KEY = "__GET__MENU__ITEMS__";
 export const GET_CUSTOMERS_KEY = "__GET__CUSTOMERS__";
 
