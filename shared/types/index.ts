e64bae5b70437362c563ecb8ddba83398ec0d9ba
Kent Ntwari <designerjehovah@gmail.com>
2026-05-18 15:10:40 -0400

    feat: add order related customer and metadata types

--- shared/types/index.ts
+++ shared/types/index.ts
@@ -1,5 +1,10 @@
 import type { User as UserModel } from "@prisma/client";
+import type {
+  TCustomerSchema as TCustomerDTO,
+  TAddressSchema,
+} from "../utils/schemas.zod";
 
+import { OrderTransformer } from "../../mvc/transformers/order";
 export interface IUserMeta {
   status: UserModel["status"];
   role: UserModel["role"];
@@ -9,3 +14,17 @@ export interface IMenuItemsCustomRequestHeaders {
   "X-Update-Title"?: "true" | "false";
   "X-Update-Pricing"?: "true" | "false";
 }
+
+export type TOrderRelatedCustomer = Pick<
+  TCustomerDTO,
+  "fullName" | "phone" | "email"
+>;
+
+export type TPreviewedOrderMetadata = {
+  count: ReturnType<typeof OrderTransformer.toCountMetadata>;
+  delivery: {
+    isRequested: boolean;
+    fee: string;
+    address: TAddressSchema | null;
+  };
+};
