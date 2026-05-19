6bee7640794507078691644beee02ad04d2c641b
Kent Ntwari <designerjehovah@gmail.com>
2026-05-19 13:36:44 -0400

    feat: implement updates to customer address

--- mvc/repository/customer.ts
+++ mvc/repository/customer.ts
@@ -1,7 +1,8 @@
-import { Prisma } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 import { db as dbClient } from "../../server/utils/db";
 import { DatabaseError } from "../errors.db";
+import { DeliveryRepository, type DeliveryAddressModel } from "./delivery";
 
 export type CustomerModel = Prisma.CustomerGetPayload<{
   omit: {
@@ -19,32 +20,38 @@ export const RepositoryFailuresMessages = {
   getCustomers: "Failed to get customers from database",
   getCustomer: "Failed to get customer from database",
   createCustomer: "Failed to create customer in database",
+  updateCustomerAddress: "Failed to update customer address in database",
 } as const;
 
-type TCreateCustomerParam = Prisma.CustomerGetPayload<{
+type TCreateCustomerParam = { id?: string } & Prisma.CustomerGetPayload<{
   select: { name: true; email: true; phone: true; whatsappPhoneNumber: true };
 }> & {
-  address?: Prisma.CustomerGetPayload<{
-    select: {
-      street: true;
-      city: true;
-      state: true;
-      postalCode: true;
-      country: true;
+    address?: {
+      street: string;
+      city: string;
+      state: string;
+      postalCode: string;
+      country: string;
     };
-  }>;
-};
-interface ICustomerRepository {
+  };
+export interface ICustomerRepository {
   getCustomers(): Promise<CustomerModel[]>;
   getCustomer(id: string): Promise<CustomerModel | null>;
   createCustomer(data: TCreateCustomerParam): Promise<CustomerModel>;
+  updateCustomerAddress(
+    customerId: string,
+    data: NonNullable<TCreateCustomerParam["address"]>,
+  ): Promise<CustomerModel>;
   // updateCustomer(
   //   data: Partial<Omit<CustomerModel, "id">>,
   // ): Promise<CustomerModel>;
 }
 
 export class CustomerRepository implements ICustomerRepository {
-  constructor(private db = dbClient) {}
+  constructor(
+    private db: PrismaClient | Prisma.TransactionClient = dbClient,
+    private deliveryRepository: DeliveryRepository = new DeliveryRepository(db),
+  ) {}
 
   async getCustomers(): ReturnType<ICustomerRepository["getCustomers"]> {
     try {
@@ -83,10 +90,17 @@ export class CustomerRepository implements ICustomerRepository {
           email: data.email,
           phone: data.phone,
           whatsappPhoneNumber: data.whatsappPhoneNumber,
-          street: data.address?.street || null,
-          city: data.address?.city || null,
-          state: data.address?.state || null,
-          postalCode: data.address?.postalCode || null,
+          ...(data.address && {
+            homeAddress: {
+              create: {
+                street: data.address.street,
+                city: data.address.city,
+                state: data.address.state,
+                postalCode: data.address.postalCode,
+                country: data.address.country,
+              },
+            },
+          }),
         },
       });
     } catch (error) {
@@ -95,6 +109,38 @@ export class CustomerRepository implements ICustomerRepository {
         customerData: data,
         error,
       });
+    }
+  }
+
+  async updateCustomerAddress(
+    customerId: string,
+    data: NonNullable<TCreateCustomerParam["address"]>,
+  ) {
+    try {
+      return await this.db.customer.update({
+        where: { id: customerId },
+        data: {
+          homeAddress: {
+            create: {
+              street: data.street,
+              city: data.city,
+              state: data.state,
+              postalCode: data.postalCode,
+              country: data.country,
+            },
+          },
+        },
+      });
+    } catch (error) {
+      throw new DatabaseError(
+        RepositoryFailuresMessages.updateCustomerAddress,
+        {
+          operation: "updateCustomerAddress",
+          customerId,
+          addressData: data,
+          error,
+        },
+      );
     }
   }
 }
